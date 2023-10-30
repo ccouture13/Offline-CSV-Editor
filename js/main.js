25e811bc37f14887b9e746df67d7bc51773bc9ec
Chris <chris.r.couture@gmail.com>
2023-10-30 17:03:25 -0400

    Add code & UI for hashing.

--- js/main.js
+++ js/main.js
@@ -20,6 +20,7 @@ function saveFile(data, filename) {
 
 let headers = [];
 let updatedHeaders = [];
+let firstRowValues = [];
 
 function handleFileUpload() {
     const fileInput = document.getElementById('csv-file');
@@ -30,6 +31,7 @@ function handleFileUpload() {
             const csvData = event.target.result;
             detectDelimiter(csvData);  
             headers = csvData.split('\n')[0].split(detectedDelimiter); 
+            firstRowValues = csvData.split('\n')[1].split(detectedDelimiter); // Store first row values
             displayHeaders();
         };
         reader.onerror = function(event) {
@@ -44,6 +46,11 @@ function parseHeaders(csvData) {
     displayHeaders();
 }
 
+// Function to check if a value looks like a SHA-256 hash
+function isHashed(value) {
+    return /^[a-fA-F0-9]{64}$/.test(value);
+}
+
 function displayHeaders() {
     const columnsContainer = document.getElementById('columns-container');
     columnsContainer.innerHTML = '';
@@ -51,11 +58,13 @@ function displayHeaders() {
     headerRow.innerHTML = `
         <th>Index</th>
         <th>Header</th>
-        <th>Modifier</th>
+        <th>Modify Header</th>
         <th>New Header</th>
+        <th>Hash Value</th>
     `;
     columnsContainer.appendChild(headerRow);
     headers.forEach((header, index) => {
+        const isHashed = isValueHashed(firstRowValues[index]);
         const columnItem = document.createElement('tr');
         columnItem.className = 'column-item';
         columnItem.innerHTML = `
@@ -75,6 +84,11 @@ function displayHeaders() {
                     <input type="text" id="new-name-${index}" placeholder="Enter New Header" disabled>
                 </div>
             </td>
+            <td>
+                <div title="${isHashed ? 'Value is already hashed' : ''}">
+                    <input type="checkbox" id="hash-${index}" ${isHashed ? 'disabled' : ''}>
+                </div>
+            </td>
         `;
         columnsContainer.appendChild(columnItem);
     });
@@ -85,6 +99,10 @@ function displayHeaders() {
     document.getElementById('file-upload-container').style.display = 'none';
 }
 
+function isValueHashed(value) {
+    // Assume a value is hashed if it's 64 characters long (length of a SHA-256 hash)
+    return value.length === 64;
+}
 
 function toggleRenameField(index) {
     const renameCheckbox = document.getElementById(`rename-${index}`);
@@ -102,13 +120,29 @@ function detectDelimiter(csvData) {
         detectedDelimiter = ',';
     }
     document.getElementById('detected-delimiter').textContent = detectedDelimiter;
-    document.getElementById('delimiter-container').style.display = '';
+    // document.getElementById('delimiter-container').style.display = '';
 }
 
 function toggleDelimiterField() {
     const changeDelimiterCheckbox = document.getElementById('change-delimiter-checkbox');
     const newDelimiterDropdown = document.getElementById('new-delimiter');
     newDelimiterDropdown.disabled = !changeDelimiterCheckbox.checked;
+}
+
+async function sha256(message) {
+    const msgBuffer = new TextEncoder().encode(message);                    
+    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+
+    return hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+}
+
+async function hashColumn(columnData) {
+    const hashedColumn = [];
+    for (const value of columnData) {
+        hashedColumn.push(await sha256(value));
+    }
+    return hashedColumn;
 }
 
 function saveUpdatedFile() {
@@ -125,10 +159,23 @@ function saveUpdatedFile() {
     const fileInput = document.getElementById('csv-file');
     const file = fileInput.files[0];
     const reader = new FileReader();
-    reader.onload = function(event) {
+    reader.onload = async function(event) {
         let updatedCsvData = event.target.result;
         const rows = updatedCsvData.split('\n');
         rows[0] = updatedHeaders.join(',');
+
+        const columns = rows.slice(1).map(row => row.split(','));
+        for (let index = 0; index < headers.length; index++) {
+            const hashCheckbox = document.getElementById(`hash-${index}`);
+            if (hashCheckbox.checked) {
+                const columnData = columns.map(row => row[index]);
+                const hashedColumn = await hashColumn(columnData);
+                columns.forEach((row, rowIndex) => {
+                    row[index] = hashedColumn[rowIndex];
+                });
+            }
+        }
+        rows.splice(1, rows.length - 1, ...columns.map(row => row.join(',')));
         updatedCsvData = rows.join('\n');
 
         const newDelimiterCheckbox = document.getElementById('change-delimiter-checkbox');
@@ -138,17 +185,15 @@ function saveUpdatedFile() {
         }
 
         // Saving updated file
-        const updatedFilename = 'updated_' + file.name;
+        const now = new Date();
+        const dateTimeString = now.toISOString().replace(/:/g, '-');  // Replace colons to ensure a valid filename
+        const updatedFilename = `${dateTimeString}_${file.name}`;
         saveFile(updatedCsvData, updatedFilename);
     };
     reader.onerror = function(event) {
         errorHandler(new Error('Failed to read file.'));
     };
     reader.readAsText(file);
-}
-
-function dragOverHandler(ev) {
-    ev.preventDefault();
 }
 
 function dropHandler(ev) {
